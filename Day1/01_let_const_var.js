4789a239c5e6a08b820755625d9cdd3852005dd9
Adarsh Kumar <adarshkr.227@gmail.com>
2025-02-11 13:15:56 +0530

    learning about let,var,const and dataType

---
+++ Day1/01_let_const_var.js
@@ -0,0 +1,22 @@
+console.log("JavaScript");
+
+
+const accountId = 12242
+let accounEmail = "abc@gamil.com"
+var accountPassword = "1234"
+let accountState;
+
+accountCity = "Jaipur"
+
+// accountId  = 2    //not allowed
+
+accounEmail = "123@gmail.com"
+accountPassword = "21212121"
+
+/*
+Prefer to not use var
+because of issue in block scope and functional scipe
+*/
+
+console.log(accountId);
+console.table([accountId, accounEmail, accountPassword, accountCity, accountState]);
---
+++ Day1/02_DatatypeAndConversion.js
@@ -0,0 +1,27 @@
+"use strict";  //treate all JS code as newwer veriosn
+
+// alert("Hello") 
+
+let name = "javascript"
+let age = 19
+let isLoggedeIn = false
+let state = null
+
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value (empty)
+// undefibned => value not assigned but it may assign
+// Symbol => unique
+
+
+// object
+
+console.log(typeof "Javascript")
+console.log(typeof 32)
+console.log(typeof 10.5)
+console.log(typeof "234run")
+console.log(typeof null)
+console.log(typeof undefined)
